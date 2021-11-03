2087b615cb1e35e721fe3dd2c2ce0d73f9aa0c3b
Lázaro <bellochlazaro.smr@gmail.com>
2021-11-03 21:04:29 +0100

    BBDD GestionSimples_2 y Ciclisme con dudas

--- BBDD/BBDD_Ciclisme.sql
+++ BBDD/BBDD_Ciclisme.sql
@@ -1,22 +1,61 @@
 /*1. Mostra l’altura total que es puja entre tots els ports de la volta.*/
+SELECT SUM(altura) AS Total_Subido
+FROM ports;
 
 /*2. Mostra l’alçada total dels ports que es pugen en l’etapa 10.*/
+SELECT SUM(altura) AS Altura_Total_Etapa_10
+FROM ports
+WHERE etapa = 10;
 
 /*3. Mostra la mitja de la pendent dels ports que on ha passat primer algun ciclista de l’equip 
 «Banesto».*/
+SELECT AVG(altura) AS Altura_Media, ciclistes.nom
+FROM ports
+LEFT JOIN ciclistes ON dorsal=ciclista
+WHERE equip LIKE 'Banesto' GROUP BY ciclistes.nom;
+
+SELECT AVG(altura) AS Altura_Media, etapa
+FROM ports
+LEFT JOIN ciclistes ON dorsal=ciclista
+WHERE equip LIKE 'Banesto' GROUP BY etapa;
 
 /*4. Mostra el port més alt que ha guanyat algun corredor mentre portava el mallot de la 
 muntanya (MMO).*/
 
 /*5. Mostra una llista amb els guanyadors d’etapa i quantes etapes han guanyat.*/
-
+SELECT ciclistes.nom, COUNT(etapes.ciclista) AS Cant_Etapas
+FROM ciclistes
+INNER JOIN etapes ON etapes.ciclista = ciclistes.dorsal
+GROUP BY ciclistes.nom;
 /*6. Mostra una llista amb els corredors de l’equip «Banesto» que han passat primers per algun 
 port i quants ports son.*/
 
+SELECT  COUNT(ciclistes.nom) AS Cantidad_Puertos, ciclistes.nom
+FROM ciclistes
+INNER JOIN ports ON ciclistes.dorsal = ports.ciclista
+WHERE ciclistes.equip LIKE 'Banesto'
+GROUP BY ciclistes.nom;
+
 /*7. Mostra una llista amb els corredors que han guanyat més d’una etapa. */
+SELECT etapes.ciclista
+FROM ciclistes
+INNER JOIN etapes ON etapes.ciclista=ciclistes.dorsal
+GROUP BY etapes.ciclista
+HAVING COUNT(etapes.ciclista) >1;
+
 
 /*8. Mostra una llista amb les etapes on entre tots els ports es puja un total de més de 1200m.*/
 
+SELECT  etapes.numero, SUM(ports.altura) Altura
+FROM etapes
+INNER JOIN ports ON etapes.numero = ports.etapa
+GROUP BY etapes.numero
+HAVING SUM(ports.altura) > 1200;
+
 /*9. Mostra una llista amb TOTS els equips i el número de corredors que tenen.*/
+SELECT equips.nom, COUNT(ciclistes.equip) AS CANT_Ciclistas
+FROM equips
+LEFT JOIN ciclistes ON ciclistes.equip = equips.nom
+GROUP BY equips.nom;
 
 /*10. Mostra una llista amb TOTS els equips i el número d’etapes que han guanyat*/
--- BBDD/GestionSimples_2.sql
+++ BBDD/GestionSimples_2.sql
@@ -1,8 +1,7 @@
 /*Consultas multitabla*/
 /*1. Modifica la definición de la tabla pedidos para que las columnas rep, clie, fab y producto no 
 admitan valores nulos. Haz lo mismo con la columna repclie de la tabla Clientes. */
-/*2.  Listar los códigos y nombres de los empleados de las oficinas del Este con su oficina y 
-ciudad. */
+/*2.  Listar los códigos y nombres de los empleados de las oficinas del Este con su oficina y ciudad. */
 SELECT empleados.numemp AS Código, empleados.nombre, oficinas.oficina, oficinas.ciudad, oficinas.region
 FROM empleados
 INNER JOIN oficinas 
@@ -34,25 +33,74 @@ FROM (pedidos INNER JOIN clientes ON pedidos.clie =clientes.numclie)
 INNER JOIN empleados
 ON clientes.repclie = empleados.numemp
 WHERE pedidos.importe > 250;
+SELECT pedidos.*, empleados.nombre AS Vendedor, clientes.nombre AS Cliente
+FROM pedidos
+INNER JOIN empleados ON numemp = rep
+INNER JOIN clientes ON numclie = clie
+WHERE pedidos.importe > 250;
 /*7. Listar los pedidos superiores a 250 euros, mostrando además el nombre del cliente que 
-solicitó  el pedido y el nombre del vendedor asignado a ese cliente y la ciudad de la oficina 
-donde el vendedor trabaja.*/
+solicitó  el pedido y el nombre del vendedor asignado a ese cliente y la ciudad de la oficina donde el vendedor trabaja.*/
+SELECT pedidos.*, clientes.nombre AS Cliente, empleados.nombre AS Vendedor, oficinas.ciudad AS Ciudad
+FROM pedidos
+INNER JOIN clientes ON pedidos.clie = clientes.numclie
+INNER JOIN empleados ON empleados.numemp = clientes.repclie
+LEFT JOIN oficinas ON empleados.oficina = oficinas.oficina
+WHERE pedidos.importe > 250;
 /*8. Hallar los pedidos recibidos los días en que un nuevo empleado fue contratado. */
+SELECT numpedido, fechapedido, rep, numemp, nombre, contrato  
+FROM pedidos,  empleados  
+WHERE fechapedido=contrato;
 /*9. Listar los empleados que tienen una cuota superior al objetivo de al menos una oficina. La 
 oficina puede ser cualquiera no tiene porqué ser la del empleado.*/
+SELECT numemp, nombre, cuota, empleados.oficina AS [Su oficina], oficinas.oficina, objetivo 
+FROM empleados, oficinas 
+WHERE cuota > objetivo;
 /*10. Listar los empleados con una cuota superior a la de su jefe, los campos a obtener son el 
 número, nombre y cuota del empleado y número, nombre y cuota de su jefe.*/
+SELECT empleados.numemp, empleados.nombre, empleados.cuota, jefes.numemp AS Jefe, jefes.nombre AS [Nombre Jefe], jefes.cuota AS [Cuota Jefe]
+FROM empleados
+INNER JOIN empleados AS jefes ON empleados.jefe = jefes.numemp
+WHERE empleados.cuota > jefes.cuota;
 /*11. Desde el entorno gráfico cambia el empleado 111, asígnale el jefe 110 y la oficina 21. 
 Después cambia la sentencia anterior para que salgan también los empleados cuyo jefe no 
 tenga cuota.*/
-/*12. Listar las oficinas (código) que no 
-tienen empleados, resolver el 
-problema de dos formas (con JOIN y 
-sin utilizar la composición de tablas).*/
-/*13. Obtener los productos (código 
-completo) que no aparecen en ningún 
-pedido. */
-/*14. Obtener los empleados de 
-GestionSimples que aparecen en 
-GestionA con otra oficina.*/
+SELECT empleados.numemp, empleados.nombre, empleados.cuota, jefes.numemp AS Jefe, jefes.nombre AS [Nombre Jefe], jefes.cuota AS [Cuota Jefe]
+FROM empleados
+INNER JOIN empleados AS jefes ON empleados.jefe = jefes.numemp
+WHERE empleados.cuota > jefes.cuota OR jefes.cuota IS NULL;
+/*12. Listar las oficinas (código) que no tienen empleados, resolver el 
+problema de dos formas (con JOIN y sin utilizar la composición de tablas).*/
+SELECT oficinas.oficina
+FROM oficinas
+LEFT JOIN empleados ON oficinas.oficina = empleados.oficina
+WHERE empleados.numemp IS NULL;
+
+SELECT oficina 
+FROM oficinas 
+EXCEPT 
+SELECT oficina 
+FROM empleados;
+/*13. Obtener los productos (código completo) que no aparecen en ningún pedido. */
+SELECT idfab, idproducto 
+FROM productos 
+EXCEPT 
+SELECT fab, producto 
+FROM pedidos;
+/*14. Obtener los empleados de GestionSimples que aparecen en GestionA con otra oficina.*/
+SELECT numemp, oficina 
+FROM gestionsimples.dbo.empleados 
+EXCEPT 
+SELECT numemp, oficina 
+FROM gestionA.dbo.empleados; 
+/*Hace dos cosnsultas independientes, como si tuviera la primera ; al final */
+SELECT numemp, oficina 
+FROM gestionsimples.dbo.empleados
+SELECT numemp, oficina 
+FROM gestionA.dbo.empleados; 
+/* Obtener los empleados de GestionSimples que aparecen en GestionA con la misma oficina.*/
+SELECT numemp, oficina 
+FROM gestionsimples.dbo.empleados
+INTERSECT
+SELECT numemp, oficina 
+FROM gestionA.dbo.empleados; 
 
