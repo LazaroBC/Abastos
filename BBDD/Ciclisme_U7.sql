5da276501dda716350b74bdab5b23439aa694547
Lázaro <bellochlazaro.smr@gmail.com>
2021-11-18 18:58:12 +0100

    BBDD Evaluable

--- BBDD/Ciclisme_U7.sql
+++ BBDD/Ciclisme_U7.sql
@@ -6,6 +6,10 @@ FROM ports
 ORDER BY altura DESC;
 
 /*2. Mostra una llista amb les dades dels ports que son més alts que tots els port de l’etapa 10;*/
+SELECT  p1.*
+FROM ports p1
+WHERE p1.altura > ALL (SELECT altura FROM ports p2 WHERE p2.etapa = 10);
+
 
 /*3. Mostra les dades del ciclista que ha guanyat més etapes.*/
 SELECT  TOP 1 ciclistes.nom, ciclistes.dorsal, ciclistes.edat, ciclistes.equip,  COUNT(etapes.ciclista) etapas
@@ -16,20 +20,67 @@ ORDER BY etapas DESC;
 
 /*4. Mostra una llista amb les dades ciclistes que corren al mateix equip que el ciclista  que ha 
 guanyat més etapes.*/
-
+SELECT ciclistes.*
+FROM ciclistes
+INNER JOIN equips ON ciclistes.equip  = equips.nom
+INNER JOIN etapes ON etapes.ciclista = ciclistes.dorsal 
+WHERE EXISTS (SELECT TOP 1 count(etapes.ciclista) 
+                            FROM etapes
+                            GROUP BY etapes.ciclista) 
+;
 /*5. Mostra una llista amb la informació dels ciclistes que corren al mateix equip que el ciclista 
 que ha guanyat més etapes i que han portat algun mallot (nom dels ciclistes, la etapa i color 
 del mallot que portaven).*/
+SELECT DISTINCT ciclistes.nom, portar.etapa, portar.mallot, ciclistes.equip
+FROM ciclistes
+INNER JOIN equips ON ciclistes.equip  = equips.nom
+INNER JOIN etapes ON etapes.ciclista = ciclistes.dorsal
+INNER JOIN portar ON ciclistes.dorsal = portar.ciclista
+WHERE EXISTS (SELECT TOP 1 count(etapes.ciclista) 
+                            FROM etapes
+                            GROUP BY etapes.ciclista)  AND ciclistes.equip ='Banesto';
 
 /*6. Mostra les dades de l’etapa amb la major  mitja d’altura entre tots els seus ports.*/
+SELECT etapes.*, Media
+FROM
+(SELECT etapa, AVG(ports.altura) AS Media
+FROM ports
+GROUP BY etapa) AS medias
+INNER JOIN etapes ON etapes.numero = medias.etapa
+WHERE Media >= ALL (SELECT AVG(ports.altura)
+FROM ports
+GROUP BY etapa);
 
 /*7. Mostra les dades dels ciclistes (son 3) amb major edat.*/
+SELECT TOP 1 WITH TIES ciclistes.*
+FROM ciclistes
+ORDER BY ciclistes.edat DESC;
 
 /*8. Mostra les dades dels ciclistes amb més edat que la edat mitja de tots els ciclistes.*/
+SELECT ciclistes.nom, ciclistes.equip, edat
+FROM ciclistes
+WHERE ciclistes.edat > ALL (SELECT  AVG(ciclistes.edat) 
+							FROM ciclistes)
+GROUP BY  ciclistes.equip, ciclistes.nom, edat;
 
 /*9. Mostra les dades dels ciclistes amb major edat de cada equip.*/
+SELECT c1.nom, c1.equip, edat
+FROM ciclistes c1
+WHERE c1.edat >= ALL (SELECT MAX(c2.edat) 
+							FROM ciclistes c2
+							WHERE c2.equip = c1.equip)
+GROUP BY  c1.equip, c1.nom, edat;
+
 
 /*10. Mostra les dades dels ciclistes amb major edat que la mitja d’edat del seu equip.*/
 
+
 /*11. Mostra les dades de tots els ciclistes dels equips on corren els ciclistes amb major edat (els 
 de la consulta 7)*/
+
+SELECT *
+FROM ciclistes 
+WHERE equip IN (SELECT equip
+FROM ciclistes
+WHERE edat >= ALL (SELECT edat FROM ciclistes));
+
---
+++ BBDD/Evaluable.sql
@@ -0,0 +1,65 @@
+/*EVALUABLE*/
+/*1. Llistat amb el nom de tots els llibres que ha demanat l’usuari 'JUALOR'.*/
+SELECT libros.nombre
+FROM libros
+INNER JOIN pide ON libros.id_libro = pide.id_libro
+INNER JOIN usuarios ON pide.usuario = usuarios.usuario
+WHERE usuarios.usuario LIKE 'JUALOR';
+
+/*2. Llistat amb el nom i la data de tots els llibres demanats en l’any 2018. */
+SELECT libros.nombre, pide.fecha
+FROM libros
+INNER JOIN pide ON pide.id_libro = libros.id_libro
+WHERE YEAR(pide.fecha) = 2018;
+
+/*3. Llistat de llibres (nom y quantitat) cedits per l’usuari 'JUAPAS'.*/
+SELECT libros.nombre, cede.cantidad 
+FROM libros
+INNER JOIN cede ON cede.id_libro = libros.id_libro
+INNER JOIN usuarios ON usuarios.usuario = cede.usuario
+WHERE usuarios.usuario = 'JUAPAS'
+GROUP BY libros.nombre, cede.cantidad;
+
+/*4. Llistat d’usuaris que encara no han demanat cap llibre.*/
+SELECT usuarios.usuario
+FROM usuarios
+LEFT JOIN pide ON pide.usuario = usuarios.usuario
+WHERE usuarios.usuario <> ALL (SELECT usuario FROM pide);
+SELECT usuarios.usuario FROM usuarios
+ORDER BY usuario 
+DESC;
+SELECT pide.usuario FROM pide
+ORDER BY usuario 
+DESC;
+
+/*5. Quantitat total de llibres, independentment del títol, que ha demanat cada usuari.*/
+SELECT usuarios.usuario, COUNT(pide.usuario) Cantidad
+FROM libros
+INNER JOIN pide ON pide.id_libro = libros.id_libro
+INNER JOIN usuarios ON usuarios.usuario = pide.usuario
+GROUP BY usuarios.usuario, pide.usuario;
+
+/*6. Quantitat total de llibres, independentment del títol, que ha cedit cada usuari.*/
+/*7. Nom i nombre d’exemplars cedits de cada llibre (només la informació de 'cede',
+ignoreu els llibres de «balance» o de 'cantidad' a la taula 'libros').*/
+/*8. Quin és el llibre/s del que s’han cedit més còpies i quantes son?*/
+/*9. Quin és el llibre/s del que s’han demanat menys còpies i quantes son?*/
+/*10. Llistat amb el total de llibres demanats pels usuaris de cada ciutat, amb subtotals per
+cada usuari i el total absolut de llibres demanats.*/
+/*11. Mitja de la qualificació que donen als llibres que han demanat els usuaris de
+'Valencia'.*/
+/*12. Llistat amb l’usuari o usuaris que han demanat més llibres i quina es eixa quantitat.*/
+/*13. Llistat d’usuaris que son de la mateixa ciutat o ciutats que l’usuari que ha cedit el
+llibre 'RVJ7973'.*/
+/*14. Usuari, 'id_libro' i qualificació de les persones que han demanat un llibre al que han
+donat la qualificació més alta que el llibre ha rebut (no =5, la qualificació més alta del
+llibre a la taula 'pide', si només en té una, podria ser inclús un 0).*/
+/*15. Fes que l’usuari 'ROSIBA' haja cedit 3 còpies del llibre '1984'. Pots triar la qualificació
+que li posa.*/
+/*16. Actualitza TOTS els llibres sense autor per tal que pose 'Anonimo'*/
+/*17. Borra les peticions de llibres de l’usuari 'LUIPAR'.*/
+/*18. Actualitza el 'balance' de cada usuari restant 2 unitats a l’actual.*/
+/*19. Actualitza de nou el 'balance' de cada usuari per restar-li a cadascun el nombre de
+llibres que ha demanat.*/
+/*20. Actualitza el 'balance' per última vegada per sumar ara la quantitat de llibres que ha
+cedit (si ha cedit 3 còpies d’un mateix llibre conten com a 3, no com a 1).*/
